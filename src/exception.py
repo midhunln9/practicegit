34402380b7a214db4d566c5f9ac09e68784a81b9
midhunln9 <midhunln9@gmail.com>
2025-07-24 15:38:29 +0530

    Day 3

--- src/exception.py
+++ src/exception.py
@@ -5,7 +5,7 @@ def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     lineno=exc_tb.tb_lineno
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+    error_message="Error occured in Python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,lineno,str(error))
 
     return error_message
@@ -19,3 +19,10 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+
+if __name__=="__main__":
+    try:
+        1/0
+    except Exception as e:
+        logging.error("Exception occurred")
+        raise CustomException(e,sys) 
